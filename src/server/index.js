5d17ef42af0ac641605174c615721a2ec39d63d8
Nicolás Albornoz <nico.albornoz.s@gmail.com>
2022-01-29 13:59:29 -0500

    feat: Setup server

--- src/server/index.js
+++ src/server/index.js
@@ -1,21 +1,27 @@
 var path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = requiere('cors')
 const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
 
 app.use(express.static('dist'))
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 
 console.log(__dirname)
+
+// designates what port the app will listen to for incoming requests
+const port = 8080
+app.listen(port, ()=>{
+    console.log(`Server running on localhost: ${port}`)
+})
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
-})
-
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
 })
 
 app.get('/test', function (req, res) {
